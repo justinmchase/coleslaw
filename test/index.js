31c9e72a513dc70b9f51d443db86b8cbf8b9020e
Justin Chase <justin.m.chase@gmail.com>
2016-05-07 23:39:54 -0500

    Some basic crud operations

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 var path = require('path')
 var chai = require('chai')
+var sinon = require('sinon')
 var coleslaw = require('..')
 var Model = require('../lib/model')
 
@@ -18,6 +19,14 @@ describe('coleslaw', () => {
         it(path.basename(p, '.cls'), (done) => {
             coleslaw.compile(p, (err, code) => {
                 if (err) return done(err)
+                var options = {
+                    dataAccess: {
+                        create: sinon.stub(),
+                        retrieve: sinon.stub(),
+                        update: sinon.stub(),
+                        delete: sinon.stub()
+                    }
+                }
                 eval(code)
                 done()
             })
